4472ba74ade3c7527ef500b9eb1a2a5162038e0d
Sean Lynch <seanl@literati.org>
2010-03-03 19:12:37 -0800

    Restart everything on join and leave

--- src/dist_sup_dispatch.erl
+++ src/dist_sup_dispatch.erl
@@ -10,28 +10,38 @@ start_link() ->
     proc_lib:start_link(?MODULE, init, [self()]).
 
 init(Parent) ->
+    process_flag(trap_exit, true),
     case do_initialization() of
-        ok ->
-            proc_lib:init_ack(Parent, {ok, self()});
+        {ok, Pid} ->
+            proc_lib:init_ack(Parent, {ok, self()}),
+            wait(Pid);
         {error, Reason} ->
             exit(Reason)
-    end,
-    wait().
+    end.
 
 do_initialization() ->
     monitor_or_supervise(global:whereis_name(?MODULE)).
 
 monitor_or_supervise(undefined) ->
     yes = global:register_name(?MODULE, self()),
-    {ok, _Pid} = global_singleton_supervisor:start_link(),
-    ok;
+    error_logger:info_msg("dist_sup_dispatch: starting global singleton.~n"),
+    global_singleton_supervisor:start_link();
 monitor_or_supervise(Pid) when is_pid(Pid) ->
+    error_logger:info_msg("dist_sup_dispatch: monitoring ~p.~n", [Pid]),
     erlang:monitor(process, Pid),
-    ok.
+    {ok, undefined}.
 
-wait() ->
+wait(Pid) ->
     receive
+        {'EXIT', From, _Reason} when Pid =/= undefined, From =/= Pid ->
+            error_logger:info_msg("dist_sup_dispatch: Shutting down child.~n"),
+            exit(Pid, shutdown),
+            receive
+                {'EXIT', Pid, Reason} ->
+                    error_logger:info_msg("dist_sup_dispatch: Child exited with reason ~p~n",
+                                          [Reason])
+            end;
         LikelyExitMessage ->
-            error_logger:info_msg("Exiting, I got a message: ~p~n",
+            error_logger:info_msg("dist_sup_dispatch: Exiting, I got a message: ~p~n",
                                   [LikelyExitMessage])
     end.
